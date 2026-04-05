5192f8d1bfd32805f31a375737d75ac5d9c7ea30
Serialist <ba3pt@qq.com>
2026-04-05 21:34:35 +0800

    chore: move bsp

--- board/bsp_adc.h
+++
@@ -1,8 +0,0 @@
-#ifndef BSP_ADC_H
-#define BSP_ADC_H
-#include "user_lib.h"
-
-extern void init_vrefint_reciprocal(void);
-extern float32_t get_temprate(void);
-extern float32_t get_battery_voltage(void);
-#endif
--- utils/debug.c
+++ utils/debug.c
@@ -11,25 +11,3 @@
 
 #include "debug.h"
 #include "user_lib.h"
-#include <math.h>
-
-#include "tim.h"
-#include "bsp_buzzer.h"
-
-extern TIM_HandleTypeDef htim4;
-void buzzer_on(uint16_t psc, uint16_t pwm)
-{
-    __HAL_TIM_PRESCALER(&htim4, psc);
-    __HAL_TIM_SetCompare(&htim4, TIM_CHANNEL_3, pwm);
-}
-void buzzer_off(void)
-{
-    __HAL_TIM_SetCompare(&htim4, TIM_CHANNEL_3, 0);
-}
-
-// float Rampf(float x, float x0, float kmax, float kmin, float dt) :
-// {
-//     float dx = fClam (kmax * dt, x0 - x);
-
-//     return new_channel;
-// }
